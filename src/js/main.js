381a0da3cfadd140ed7879ed5550121a4f051e00
aleksmal76 <leha7.6@yandex.ru>
2018-01-07 00:15:13 +0300

    добавил стили для планшета в квталог

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,33 @@
+$(document).ready(function () {
+
+  $('.player__btn').click(function (event) {
+    event.preventDefault();
+    $('.overlay').fadeIn(400, // анимируем показ обложки
+      function () { // далее показываем мод. окно
+        $('.modal-ctg')
+          .css('display', 'block')
+          .animate({
+            opacity: 1,
+            top: '50%'
+          }, 200);
+      });
+  });
+
+  // закрытие модального окна
+  $('.modal-ctg__btn, .overlay').click(function () {
+    $('.modal-ctg')
+      .animate({
+          opacity: 0,
+          top: '45%'
+        }, 200, // уменьшаем прозрачность
+        function () { // пoсле aнимaции
+          $(this).css('display', 'none'); // скрываем окно
+          $('.overlay').fadeOut(400); // скрывaем пoдлoжку
+        }
+      );
+  });
+
+});
+
 
 

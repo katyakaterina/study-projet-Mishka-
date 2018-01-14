f48ffaf03642e3a3ab77df34ed2c87b51695a216
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2018-01-14 22:52:06 +0300

    waves

--- src/js/main.js
+++ src/js/main.js
@@ -12,27 +12,16 @@ overlay.style.display = "block";
 close.onclick = function () {
 modal.style.display = "none";
 overlay.style.display ="none";
-}
+
+
+close.keyCode = function() {
+    if(e.keyCode === 27, (event.target == modal)) {
+        overlay.style.display = "none";
+    }},
+
+
 overlay.onclick = function(event) {///должно закрываться по подложке((
 if (event.target == modal){
 modal.style.display = "none";
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}};

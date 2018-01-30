2006c6efd65c04ba0a674c159f10f129627505bc
aleksmal76 <leha7.6@yandex.ru>
2018-01-30 20:11:42 +0300

    синхронизация

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,19 +2,28 @@
 
 var buttonMenu = document.getElementById("buttonMenu");
 var menu = document.getElementById("menuList");
+var iconlose = document.getElementsByClassName('openBtn');
+var iconOpen=document.getElementsByClassName('closeBtn');
+
 var iconOpen = document.getElementById("openButton");
 var iconClose = document.getElementById("closeButton");
+
+
+
 
 buttonMenu.addEventListener("click", function (e) {
 (menu.classList.toggle("open"));
 
-   if (e) {
-     (iconClose.style.display = 'block');
-      return (iconOpen.style.display = 'none');
-    }
-      else {
-     (iconOpen.style.display = 'block');
-     return (iconClose.style.display = 'none');
-   }
+  if (e) {
+    (iconClose.style.display = 'block');
+    return (iconOpen.style.display = 'none');
+  }
+  else {
+    (iconOpen.style.display = 'block');
+    return (iconClose.style.display = 'none');
+  }
+
+
 });
+
 

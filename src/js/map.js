36131818d699c2aedcc26303acfd9d23169da106
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2018-02-15 12:11:40 +0300

    увеличен зум карты, высота слайдов отредактирована

--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,7 @@
     function initMap() {
         var uluru = {lat: 59.9388068, lng: 30.3230066};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 16,
           center: uluru
         });
         var marker = new google.maps.Marker({
